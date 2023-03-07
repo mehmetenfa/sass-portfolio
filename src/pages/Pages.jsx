cbb19250f9f3781ad3b127c44b06d4236fad927b
qxan <mehmet.enfa@gmail.com>
2023-03-07 20:32:55 +0300

    pages folder ok

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "../common/Header";
 import { Home } from "../components/home/Home";
+import { Resume } from "../components/page/Resume";
+import { Portfolio } from "../components/page/Portfolio";
+import { Blog } from "../components/page/Blog";
+import { Contact } from "../components/page/Contact";
 import { SideContent } from "../components/side/SideContent";
 
 export const Pages = () => {
@@ -8,12 +13,17 @@ export const Pages = () => {
     <>
       <div className="main-div">
         <div className="side">
-            <SideContent />
+          <SideContent />
         </div>
         <main>
           <Router>
+            <Header />
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/resume" component={Resume} />
+              <Route exact path="/portfolio" component={Portfolio} />
+              <Route exact path="/blog" component={Blog} />
+              <Route exact path="/contact" component={Contact} />
             </Switch>
           </Router>
         </main>
